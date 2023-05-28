ab932a296337b10eb5a8c9b599c0e0b0c96cf6cb
pestoff7 <ilya.pestov.03@gmail.com>
2023-05-28 16:04:08 +0400

    added hashed password

--- action/authAction.php
+++ action/authAction.php
@@ -3,19 +3,23 @@ include "connect.php";
 
 $email = $_POST['email'];
 $password = $_POST['password'];
-
-$query = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+$query = "SELECT * FROM users WHERE email = '$email'";
 $result = mysqli_query($conn, $query);
 while($row = $result->fetch_assoc()){
-  $name = $row['name'] ;
-  $surname = $row['surname'];
-  $email = $row['email'];
-  $password = $row['password'];
+  $password_from_db = $row['password'];
 }
-session_start();
-$_SESSION["name"] = $name;
-$_SESSION["surname"] = $surname;
-echo $name . $surname . $email . $password;
-header("Location: ../pages/index.php");
-
+if (password_verify($password, $password_from_db)) {
+  $query = "SELECT * FROM users WHERE email = '$email'";
+  $result = mysqli_query($conn, $query);
+  while($row = $result->fetch_assoc()){
+    $name = $row['name'] ;
+    $surname = $row['surname'];
+  }
+  session_start();
+  $_SESSION["name"] = $name;
+  $_SESSION["surname"] = $surname;
+  header("Location: ../pages/index.php");
+} else {
+  echo "Пароль неверен";
+} 
 ?>
--- action/registration.php
+++ action/registration.php
@@ -7,7 +7,9 @@ $surname = $_POST['surname'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$query = "INSERT INTO users(name, surname, email, password, isAdmin) VALUES ('$name', '$surname', '$email', '$password', 0);";
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+$query = "INSERT INTO users(name, surname, email, password, isAdmin) VALUES ('$name', '$surname', '$email', '$hashed_password', 0);";
 $result = mysqli_query($conn, $query);
 session_start();
 $_SESSION["name"] = $name;
